0d03d8865d6078800e21140462b54d4aa7fea27e
pingyang1117 <pingyang.cs08g@nctu.edu.tw>
2023-03-14 18:01:16 +0800

    sync paper

--- prb/run_e6e.sh
+++
@@ -1,1 +0,0 @@
-python train_aux.py --workers 8 --device 0 --batch-size 15 --data data/coco.yaml --epochs 300 --img 1408 1408 --cfg cfg/training/PRB_Series/yolov7-PRB-2PY-e6e-final.yaml --weights '' --name PRB-FPN6-X-1408-0105 --hyp data/hyp.scratch.p6.yaml
